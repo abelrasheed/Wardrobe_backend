706dfd72ac335bc41bdf074da472ef85b2a70ebe
Abel Rasheed <abelrasheed@gmail.com>
2021-01-29 15:32:44 +0530

    Re installed dependencies

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,11 @@
 from flask_cors import CORS
 from flask import Flask
 from flask_restful import Api
-from flask_compress import Compress
+
 
 
 app = Flask(__name__)
-Compress(app)
+
 CORS(app)
 api = Api(app)
 
